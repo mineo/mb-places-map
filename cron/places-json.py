ef7c319a83ae823dda81d84b1e6efd05e505bb6e
Wieland Hoffmann <themineo@gmail.com>
2013-10-19 10:47:45 +0200

    Remove an unused pprint import

--- cron/places-json.py
+++ cron/places-json.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 import json
 import re
-import pprint
 import psycopg2 as pg
 db = pg.connect("dbname=musicbrainz_db_slave user=musicbrainz host=127.0.0.1")
 cur = db.cursor()
-cur.execute("SELECT gid, name, coordinates FROM place WHERE coordinates IS NOT
-            NULL;")
+cur.execute("SELECT gid, name, coordinates FROM place WHERE coordinates IS NOT NULL;")
 places = {}
 for result in cur:
     coords = result[2]
@@ -15,5 +13,4 @@ for result in cur:
                          'coordinates': [float(m.group(1)), float(m.group(2))]
                          }
 with open("/home/mineo/public_html/places/places.json", "w") as fp:
-    #pprint.pprint(json.dumps(places))
     json.dump(places, fp)
