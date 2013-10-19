39cf7e43ab61483846c65de8323839af39b7d100
Wieland Hoffmann <themineo@gmail.com>
2013-10-19 10:49:44 +0200

    Make the json file path an option

--- cron/places-json.py
+++ cron/places-json.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+"""
+Usage: places-json.py path-to-target-json-file
+"""
 import json
 import re
 import psycopg2 as pg
+import sys
+
+if not len(sys.argv) == 2:
+    exit(__doc__)
+
 db = pg.connect("dbname=musicbrainz_db_slave user=musicbrainz host=127.0.0.1")
 cur = db.cursor()
 cur.execute("SELECT gid, name, coordinates FROM place WHERE coordinates IS NOT NULL;")
@@ -12,5 +20,5 @@ for result in cur:
     places[result[0]] = {'name': result[1],
                          'coordinates': [float(m.group(1)), float(m.group(2))]
                          }
-with open("/home/mineo/public_html/places/places.json", "w") as fp:
+with open(sys.argv[1], "w") as fp:
     json.dump(places, fp)
