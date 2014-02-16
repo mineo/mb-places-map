43b07b76b62b5f89b16b0a0e7c5962f25de7874e
Wieland Hoffmann <themineo@gmail.com>
2014-02-16 10:53:42 +0100

    Revert "Add clustering with the GMaps Utility Libraries MarkerClusterer"
    This reverts commit 93924b53af18a998404aa44dded2620f24ce067a.

--- src/places.js
+++ src/places.js
@@ -1,10 +1,11 @@
 var infotext = "<h3>NAMEHERE</h3><a href='//musicbrainz.org/place/MBIDHERE'>View this place on MusicBrainz</a>";
 
-function makeMarker(map, mbid, data){
+function addMarker(map, mbid, data){
     name = data['name'];
     coordinates = data['coordinates'];
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(coordinates[0], coordinates[1]),
+        map: map,
         title: name
     });
     marker.mbid = mbid;
@@ -16,7 +17,6 @@ function makeMarker(map, mbid, data){
         });
         infowindow.open(map, marker);
     });
-    return marker
 }
 
 function makeMap(){
@@ -30,12 +30,9 @@ function makeMap(){
 
 function init(){
     map = makeMap();
-    markers = [];
     $.getJSON("places.json").done(function(data){
         $.each(data, function(key, val){
-            markers.push(makeMarker(map, key, val));
+            addMarker(map, key, val);
         });
-        clusterOptions = {maxZoom:13};
-        clusterer = new MarkerClusterer(map, markers, clusterOptions);
     });
 }
