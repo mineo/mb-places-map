6e45ee3b6166d05b78489ce53023b13199537a85
Wieland Hoffmann <themineo@gmail.com>
2014-09-30 12:35:36 +0200

    Use psycopg2.extras.NamedTupleCursor

--- cron/places-json.py
+++ cron/places-json.py
@@ -5,20 +5,21 @@ Usage: places-json.py path-to-target-json-file
 import json
 import re
 import psycopg2 as pg
+import psycopg2.extras as pgex
 import sys
 
 if not len(sys.argv) == 2:
     exit(__doc__)
 
 db = pg.connect("dbname=musicbrainz_db_slave user=musicbrainz host=127.0.0.1")
-cur = db.cursor()
+cur = db.cursor(cursor_factory=pgex.NamedTupleCursor)
 cur.execute("SELECT gid, name, coordinates FROM place WHERE coordinates IS NOT NULL;")
 places = {}
 for result in cur:
-    coords = result[2]
+    coords = result.coordinates
     m = re.match(r"\(([^)]+),([^)]+)\)", coords)
-    places[result[0]] = {'name': result[1],
-                         'coordinates': [float(m.group(1)), float(m.group(2))]
-                         }
+    places[result.gid] = {'name': result.name,
+                          'coordinates': [float(m.group(1)), float(m.group(2))]
+                          }
 with open(sys.argv[1], "w") as fp:
     json.dump(places, fp)
