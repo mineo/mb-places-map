334885990e05f16866c87e9748b4e2296d2e8b8c
Wieland Hoffmann <themineo@gmail.com>
2015-01-09 17:38:31 +0100

    Use leaflet.geoJson to display data

--- src/places.js
+++ src/places.js
@@ -21,68 +21,71 @@ var tileLayers = {
 
 L.control.layers(tileLayers).addTo(map);
 
+function featureToPopup(feature){
+    mbid = feature.key;
+    name = feature.properties.name;
+    coordinates = feature.properties.coordinates
+    var marker = L.marker(coordinates, {'title': name});
+    var info = document.createElement("div");
+
+    var name = document.createElement("h3");
+    name.textContent = feature.properties.name;
+    info.appendChild(name);
+
+    if ('thumbnail_link' in feature.properties){
+        var imagelink = document.createElement("a");
+        imagelink.href = feature.properties.commons_link
+        imagelink.target = "_blank";
+
+        var image = document.createElement("img");
+        image.alt = "image of this place";
+        image.src = feature.properties.thumbnail_link;
+
+        imagelink.appendChild(image)
+
+        info.appendChild(imagelink)
+        info.appendChild(document.createElement("br"))
+
+        var commonslink = document.createElement("a");
+        commonslink.href = feature.properties.commons_link;
+        commonslink.target = "_blank";
+        info.appendChild(commonslink)
+    }
+
+    events = feature.properties.events
+    if (events.length > 0){
+        var heading = document.createElement("h4");
+        heading.textContent = "Events";
+        info.appendChild(heading);
+
+        var ev_list = document.createElement("ul")
+        $.each(events, function(index, event){
+            var link = document.createElement("a");
+            link.href = "//musicbrainz.org/event/" + event.gid;
+            link.textContent = event.name;
+
+            var li = document.createElement("li");
+            li.appendChild(link);
+            ev_list.appendChild(li);
+        })
+        info.appendChild(ev_list);
+    }
+
+    var link = document.createElement("a");
+    link.href = "//musicbrainz.org/place/" + feature.id;
+    link.textContent = "View this place on MusicBrainz";
+    info.appendChild(link);
+    return info;
+}
+
 var hash = new L.Hash(map);
 $.getJSON("places.json").done(function(data){
     var markerCluster = new L.MarkerClusterGroup();
-    var markers = [];
-    $.each(data, function(key, val){
-        mbid = key;
-        name = val['name'];
-        coordinates = val['coordinates']
-        var marker = L.marker(coordinates, {'title': name});
-        var info = document.createElement("div");
-
-        var name = document.createElement("h3");
-        name.textContent = val.name;
-        info.appendChild(name);
-
-        if ('thumbnail_link' in val){
-            var imagelink = document.createElement("a");
-            imagelink.href = val.commons_link
-            imagelink.target = "_blank";
-
-            var image = document.createElement("img");
-            image.alt = "image of this place";
-            image.src = val.thumbnail_link;
-
-            imagelink.appendChild(image)
-
-            info.appendChild(imagelink)
-            info.appendChild(document.createElement("br"))
-
-            var commonslink = document.createElement("a");
-            commonslink.href = val.commons_link;
-            commonslink.target = "_blank";
-            info.appendChild(commonslink)
+    var geoJsonLayer = L.geoJson(data, {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(featureToPopup(feature));
         }
-
-        events = val.events
-        if (events.length > 0){
-            var heading = document.createElement("h4");
-            heading.textContent = "Events";
-            info.appendChild(heading);
-
-            var ev_list = document.createElement("ul")
-            $.each(events, function(index, event){
-                var link = document.createElement("a");
-                link.href = "//musicbrainz.org/event/" + event.gid;
-                link.textContent = event.name;
-
-                var li = document.createElement("li");
-                li.appendChild(link);
-                ev_list.appendChild(li);
-            })
-            info.appendChild(ev_list);
-        }
-
-        var link = document.createElement("a");
-        link.href = "//musicbrainz.org/place/" + key;
-        link.textContent = "View this place on MusicBrainz";
-        info.appendChild(link);
-
-        marker.bindPopup(info);
-        markers.push(marker);
     });
-    markerCluster.addLayers(markers);
+    markerCluster.addLayer(geoJsonLayer);
     map.addLayer(markerCluster);
 });
